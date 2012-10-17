7091d4c536126b2ab4348face5124a0faef80a13
Jeremy Wood <farachan@gmail.com>
2012-10-17 12:35:03 -0400

    code

---
+++ src/main/java/com/dumptruckman/minecraft/util/Logging.java
@@ -0,0 +1,222 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package com.dumptruckman.minecraft.util;
+
+import com.onarandombox.util.DebugLog;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Static plugin logger.
+ */
+public class Logging {
+
+    private static Logger log = Logger.getLogger("Minecraft");
+    private static String name = "PluginBase";
+    private static String version = "v.???";
+    private static String debug = "-Debug";
+    private static int debugLevel = 0;
+    private static DebugLog debugLog = null;
+    private static Plugin plugin;
+
+    private Logging() {
+        throw new AssertionError();
+    }
+
+    /**
+     * Prepares the log for use.  Debugging will default to disabled when initialized.
+     *
+     * @param plugin The plugin using this static logger.
+     */
+    public static void init(final Plugin plugin) {
+        name = plugin.getName();
+        version = plugin.getDescription().getVersion();
+        Logging.plugin = plugin;
+        setDebugLevel(0);
+    }
+
+    /**
+     * Closes the debug log if it is open.
+     */
+    public static void close() {
+        if (debugLog != null) {
+            debugLog.close();
+            debugLog = null;
+        }
+    }
+    
+    /**
+     * Sets the debug logging level of this plugin.  Debug messages will print to the console and to a
+     * debug log file when enabled.
+     * debugLevel:
+     *   0 - turns off debug logging, disabling the debug logger, closing any open file hooks.
+     *   1 - enables debug logging of {@link java.util.logging.Level#FINE} or lower messages.
+     *   2 - enables debug logging of {@link java.util.logging.Level#FINER} or lower messages.
+     *   3 - enables debug logging of {@link java.util.logging.Level#FINEST} or lower messages.
+     *
+     * @param debugLevel 0 = off, 1-3 = debug level
+     */
+    public static void setDebugLevel(final int debugLevel) {
+        if (debugLevel > 3 || debugLevel < 0) {
+            throw new IllegalArgumentException("debugLevel must be between 0 and 3!");
+        }
+        if (debugLevel > 0) {
+            plugin.getDataFolder().mkdirs();
+            debugLog = new DebugLog(name, plugin.getDataFolder() + File.separator + "debug.log");
+        } else {
+            close();
+        }
+        Logging.debugLevel = debugLevel;
+    }
+
+    /**
+     * Returns the current debug logging level.
+     *
+     * @return A value 0-3 indicating the debug logging level.
+     */
+    public static int getDebugLevel() {
+        return debugLevel;
+    }
+
+    /**
+     * Adds the plugin name and optionally the version number to the log message.
+     *
+     * @param message     Log message
+     * @param showVersion Whether to show version in log message
+     * @return Modified message
+     */
+    public static String getPrefixedMessage(final String message, final boolean showVersion) {
+        final StringBuilder builder = new StringBuilder("[").append(name);
+        if (showVersion) {
+            builder.append(" ").append(version);
+        }
+        builder.append("] ").append(message);
+        return builder.toString();
+    }
+
+    /**
+     * Sets the debug prefix for debug messages that follows the plugin name.  The default is "-Debug".
+     *
+     * @param debugPrefix the new debug prefix to use.
+     */
+    public static void setDebugPrefix(final String debugPrefix) {
+        Logging.debug = debugPrefix;
+    }
+
+    /**
+     * Adds the plugin's debug name to the message.
+     *
+     * @param message     Log message
+     * @return Modified message
+     */
+    public static String getDebugString(final String message) {
+        return "[" + name + debug + "] " + message;
+    }
+
+    /**
+     * Returns the logger object.
+     *
+     * @return Logger object
+     */
+    public static Logger getLogger() {
+        return log;
+    }
+
+    /**
+     * Custom log method.
+     *
+     * @param level       Log level
+     * @param message     Log message
+     * @param showVersion True adds version into message
+     */
+    public static void log(final Level level, String message, final boolean showVersion, final Object...args) {
+        if (level == Level.FINE && Logging.debugLevel >= 1) {
+            debug(Level.INFO, message, args);
+        } else if (level == Level.FINER && Logging.debugLevel >= 2) {
+            debug(Level.INFO, message, args);
+        } else if (level == Level.FINEST && Logging.debugLevel >= 3) {
+            debug(Level.INFO, message, args);
+        } else if (level != Level.FINE && level != Level.FINER && level != Level.FINEST) {
+            message = String.format(message, args);
+            log.log(level, getPrefixedMessage(message, showVersion));
+            if (debugLog != null) {
+                debugLog.log(level, getPrefixedMessage(message, showVersion));
+            }
+        }
+    }
+
+    /**
+     * Directly outputs a message with the debug prefix to both the regular logger and the debug logger if one is set.
+     *
+     * @param message The message to log.
+     * @param args    Arguments for the String.format() that is applied to the message.
+     */
+    private static void debug(final Level level, String message, final Object...args) {
+        message = String.format(message, args);
+        log.log(level, getDebugString(message));
+        if (debugLog != null) {
+            debugLog.log(level, getDebugString(message));
+        }
+    }
+
+    /**
+     * Info level logging.
+     *
+     * @param message Message to log.
+     */
+    public static void fine(final String message, final Object...args) {
+        Logging.log(Level.FINE, message, false, args);
+    }
+
+    /**
+     * Finer debug level logging.  Use for somewhat frequent messages.
+     *
+     * @param message Message to log.
+     */
+    public static void finer(final String message, final Object...args) {
+        Logging.log(Level.FINER, message, false, args);
+    }
+
+    /**
+     * Finest debug level logging.  Use for extremely frequent messages.
+     *
+     * @param message Message to log.
+     */
+    public static void finest(final String message, final Object...args) {
+        Logging.log(Level.FINEST, message, false, args);
+    }
+
+    /**
+     * Info level logging.
+     *
+     * @param message Message to log.
+     */
+    public static void info(final String message, final Object...args) {
+        Logging.log(Level.INFO, message, false, args);
+    }
+
+    /**
+     * Warning level logging.
+     *
+     * @param message Message to log.
+     */
+    public static void warning(final String message, final Object...args) {
+        Logging.log(Level.WARNING, message, false, args);
+    }
+
+    /**
+     * Severe level logging.
+     *
+     * @param message Message to log.
+     */
+    public static void severe(final String message, final Object...args) {
+        Logging.log(Level.SEVERE, message, false, args);
+    }
+
+}
+
+
---
+++ src/main/java/com/onarandombox/util/DebugLog.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2012, The Multiverse Team All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of The Multiverse Team nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.onarandombox.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+/**
+ * The Multiverse debug-logger.
+ */
+public class DebugLog {
+
+    private final FileHandler fileHandler;
+    private final Logger log;
+
+    /**
+     * Creates a new debug logger.
+     *
+     * @param logger The name of the logger.
+     * @param file   The file to log to.
+     */
+    public DebugLog(final String logger, final String file) {
+        log = Logger.getLogger(logger);
+        FileHandler fh = null;
+        try {
+            fh = new FileHandler(file, true);
+            log.setUseParentHandlers(false);
+            Set<Handler> toRemove = new HashSet<Handler>(log.getHandlers().length);
+            for (Handler handler : log.getHandlers()) {
+                toRemove.add(handler);
+            }
+            for (Handler handler : toRemove) {
+                log.removeHandler(handler);
+            }
+            log.addHandler(fh);
+            log.setLevel(Level.ALL);
+            fh.setFormatter(new LogFormatter());
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (fh != null) {
+            fileHandler = fh;
+        } else {
+            fileHandler = fh;
+        }
+    }
+
+    /**
+     * Log a message at a certain level.
+     *
+     * @param level The log-{@link java.util.logging.Level}.
+     * @param msg the message.
+     */
+    public void log(Level level, String msg) {
+        log.log(level, msg);
+    }
+
+    /**
+     * Our log-{@link java.util.logging.Formatter}.
+     */
+    private static class LogFormatter extends Formatter {
+        private final SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        @Override
+        public String format(final LogRecord record) {
+            final StringBuilder builder = new StringBuilder();
+            final Throwable ex = record.getThrown();
+
+            builder.append(this.date.format(record.getMillis()));
+            builder.append(" [");
+            builder.append(record.getLevel().getLocalizedName().toUpperCase());
+            builder.append("] ");
+            builder.append(record.getMessage());
+            builder.append('\n');
+
+            if (ex != null) {
+                final StringWriter writer = new StringWriter();
+                ex.printStackTrace(new PrintWriter(writer));
+                builder.append(writer);
+            }
+
+            return builder.toString();
+        }
+    }
+
+    /**
+     * Closes this {@link com.dumptruckman.minecraft.pluginbase.util.DebugLog}.
+     */
+    public void close() {
+        log.removeHandler(fileHandler);
+        fileHandler.close();
+    }
+}
